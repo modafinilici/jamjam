3c451aed22a1a78a3490f4ad60f1bea040c1c5f9
Rekk <69039425+modafinilici@users.noreply.github.com>
2020-08-04 01:53:33 +0300

    First commit

---
+++ gridsome.config.js
@@ -0,0 +1,99 @@
+// This is where project configuration and plugin options are located. 
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+const path = require('path')
+
+function addStyleResource (rule) {
+  rule.use('style-resource')
+    .loader('style-resources-loader')
+    .options({
+      patterns: [
+        path.resolve(__dirname, './src/assets/scss/globals.scss')
+      ],
+    })
+}
+
+module.exports = {
+  siteName: 'Jamdocs',
+  siteUrl: process.env.SITE_URL || 'http://localhost:8080',
+  plugins: ['gridsome-plugin-robots'],
+  templates: {
+    Doc: '/:slug',
+  },
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'docs/**/*.md',
+        typeName: 'Doc',
+        remark: {
+          plugins: [
+            '@gridsome/remark-prismjs'
+          ]
+        }
+      }
+    },
+    {
+      use: '@gridsome/plugin-google-analytics',
+      options: {
+        id: (process.env.GA_ID ? process.env.GA_ID : 'UA-144741960-1')
+      }
+    },
+    {
+      use: '@gridsome/plugin-sitemap',
+      options: {
+        cacheTime: 600000,
+		config: {
+          "/*": {
+            changefreq: "weekly",
+            priority: 0.5,
+          },
+        },
+      },
+    },
+	{
+      use: 'klaro-gridsome',
+      options: {
+        privacyPolicy: '/privacy/',
+        cookieName: 'consent',
+        translations: {
+          en: {
+            consentModal: {
+              description: 'Here you can see and customize the information that we collect about you.',
+            },
+            googleAnalytics: {
+              description: 'Website analytics powered by Google Analytics, allowing us to see how visitors use our website.'
+            },
+            purposes: {
+              analytics: 'Analytics'
+            },
+          },
+        },
+        apps: [
+          {
+            name: 'googleAnalytics',
+            default: true,
+            title: 'Google Analytics',
+            purposes: ['analytics'],
+            cookies: [
+              '_ga',
+              '_gcl_au',
+              '_gid',
+              '_gat'
+            ],
+            required: false,
+            optOut: true,
+            onlyOnce: false
+          }
+        ]
+      }
+    },
+  ],
+  chainWebpack: config => {
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
+    types.forEach(type => addStyleResource(config.module.rule('scss').oneOf(type)))
+  }
+}
+
